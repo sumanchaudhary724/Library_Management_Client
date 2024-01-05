59ea037980b62757236377affa4a161e30c3d892
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-01-05 19:57:48 +1100

    Added backend url

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const rootAPI = "http://localhost:8000";
+const rootAPI = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
 const userAPI = rootAPI + "/api/v1/user";
 const bookAPI = rootAPI + "/api/v1/book";
 const burrowAPI = rootAPI + "/api/v1/burrow";
